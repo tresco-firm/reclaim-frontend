ebd57ac3d0cc22e381384108d100781a54dd0d17
tonythomasndm <ttonythomasndm@gmail.com>
2026-06-29 18:58:00 +0530

    Remove old SVG icons, add new logo SVG, and update Navbar and Landing components for logo integration and asset path correction.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,11 @@
 import { navLinks } from "../constants";
 
 const Navbar = () => (
-  <section className="flex flex-row justify-between px-20 py-6 ">
-    <h1 className="text-3xl font-bold text-[#004FD9]">Reclaim</h1>
+  <main className="flex flex-row justify-between px-40 py-8">
+    <section className="flex flex-row items-center gap-4" id="navbar-header">
+      <img src="/public/logo.svg" alt="Logo" className="w-10 h-10" />
+      <h1 className="text-4xl font-bold text-[#004FD9]">Reclaim</h1>
+    </section>
     <nav className="flex gap-10">
       {navLinks.map((link) => (
         <a
@@ -17,7 +20,7 @@ const Navbar = () => (
     <button className="bg-[#004FD9] text-white px-6 py-2 rounded-lg hover:bg-[#0033a1] transition-all duration-300">
       Get Started
     </button>
-  </section>
+  </main>
 );
 
 export default Navbar;
