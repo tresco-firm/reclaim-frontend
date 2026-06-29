b648ad0769ec25fb61b092a0c2503a06060dcb79
tonythomasndm <ttonythomasndm@gmail.com>
2026-06-29 23:20:09 +0530

    feat: update Questionnaires component layout and styling for improved presentation

--- src/pages/Questionnaires.tsx
+++ src/pages/Questionnaires.tsx
@@ -1,12 +1,19 @@
 const Questionnaires = () => {
   return (
-    <div>
+    <section className="flex flex-col items-center justify-center gap-10 py-10 px-45 max-md:py-5 max-md:px-15">
       <img
         src="/hero-questionnaire.png"
         alt="Questionnaire"
-        className="w-full h-auto"
+        className="w-[50%] h-auto "
       />
-    </div>
+      <h1 className="text-4xl font-bold text-center max-md:text-2xl">
+        Let's Understand Your <br /> Relationship With Technology
+      </h1>
+      <p className="text-lg text-center">
+        This isn't a test. It's the first step toward reclaiming your life.
+      </p>
+      <button className="button">Begin my recovery assessment!</button>
+    </section>
   );
 };
 
